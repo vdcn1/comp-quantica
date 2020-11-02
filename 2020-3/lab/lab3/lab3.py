63de7fcc09b666b04987150a208046986354a16f
vdcn1 <veetsirrah@gmail.com>
2020-11-02 16:20:36 -0300

    primeira feito, fazendo a segunda

--- 2020-3/lab/lab3/lab3.py
+++ 2020-3/lab/lab3/lab3.py
@@ -1,7 +1,11 @@
 import qiskit
-
+import numpy as np
 def inv_media(vetor):
-    inv_media_vetor = 0
+    media = np.mean(vetor)
+    for i in range(len(vetor)):
+        dist = vetor[i] - media
+        vetor[i] = media - dist
+    inv_media = vetor
     return inv_media
 
 def qinv_media(n):
@@ -9,7 +13,24 @@ def qinv_media(n):
     :param n: número de qubits
     :return: circuit inversão sobre a média
     '''
-    circuito = qiskit.QuantumCircuit(n)
+    q = qiskit.QuantumRegister(n, name = 'q')
+    circuito = qiskit.QuantumCircuit(q)
+
+    for i in range(n):
+        circuito.h(q[i])
+
+    circuito.h([0,1,2])
+    circuito.z([0,1,2])
+    circuito.cz(0,1)
+    circuito.cz(1,2)
+    circuito.h([0,1,2])
+
+
+    for i in range(n):
+        circuito.h(q[i]) 
+    circuito.draw()
+
+    print(circuito)
     return circuito
 
 def oraculo_trivial(n, k):
